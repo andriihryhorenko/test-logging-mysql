b80163f8c73cb4d5b9f89ce60d57911994780e34
Andrey-JA <112312750+andriihryhorenko@users.noreply.github.com>
2022-11-17 14:06:38 +0200

    updates

---
+++ myssql-.netcore-test-app/Migrations/20221117112518_MyFirstMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myssql_.netcore_test_app.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
